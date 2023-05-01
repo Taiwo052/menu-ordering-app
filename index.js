ca8ac0893a5e57cfb7ccd920007e7a9b8b843f82
Aloba Taiwo <98100387+Taiwo052@users.noreply.github.com>
2023-05-01 16:02:06 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,127 @@
 import { menuArray } from './data.js'
-// import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
-//   console.log(uuidv4());
-let hasOrdered = false
+
+const paymentForm = document.getElementById('payment-form')
+const completeOrderBtn = document.getElementById('complete-order-btn')
+const itemArray = []
 
 document.addEventListener( 'click', function(e){
-    if(e.target.dataset.btn){
-        addItem(e.target.dataset.btn)
+    if(e.target.dataset.addBtn){
+        addItem(e.target.dataset.addBtn)
+        document.getElementById(`appreciation-container`).style.display = 'none'
+        render()
+    }else if(e.target.dataset.removeBtn){
+        removeItem(e.target.dataset.removeBtn)
+        render()
     }
 })  
 
+completeOrderBtn.addEventListener( 'click', function(){
+    document.getElementById(`payment-container`).style.display = 'inline'
+})
+
+paymentForm.addEventListener( 'submit', function(e){
+    e.preventDefault()
+    document.getElementById(`payment-container`).style.display = 'none'
+    document.getElementById(`order-container`).style.display = 'none'
+    document.getElementById(`appreciation-container`).style.display = 'inline'
+    
+    // setTimeout(function(){
+    //     document.getElementById(`appreciation`).style.display = 'inline'
+    // }, 1500)
+})
+
 function addItem(item){
     let orderObj = {}
-    let itemArray = []
 
-     orderObj = menuArray.filter(function(menu){
+    orderObj = menuArray.filter(function(menu){
     return menu.id == item || menu.name == item
 })[0]
-    itemArray.push(orderObj)
-    getOrder(item)
+        if(itemArray.includes(orderObj) ){
+        orderObj.quantity++   
+    }else{
+        itemArray.push(orderObj)
+        orderObj.quantity++
+    }
+    orderObj.quantity += !orderObj.quantity
+    theTotalPrice()
+    getOrder(itemArray)
+
+    render()
+    //  console.log(orderObj.price)
+}
+
+function removeItem(item) {
+    console.log(itemArray.indexOf(item))
+    
+    let i = itemArray.indexOf(item);
+    itemArray.splice(i);
+    console.log(itemArray)
+     itemTotalPrice()
+     getOrder()
+     render()
+};
+
+
+function itemTotalPrice(){
+    let itemPrice = theTotalPrice()--
+    console.log(itemPrice)
+}
+
+function getTotalPrice(){
+     let totalPrice = 0
+    // I loop through the itemArray to get the total price
+    itemArray.forEach(function(menu){
+            totalPrice += menu.price*menu.quantity
+    })
+    return totalPrice
+}
+
+function theTotalPrice(){
+    document.getElementById('total-price').innerHTML = `$${getTotalPrice()}`
 }
 
 function getOrder(){
-    document.getElementById(`order-container`).classList.toggle('hidden')
-    getItem()
+    renderOrder()
+    document.getElementById(`order-container`).style.display = 'flex'
+
 }
 
 function getItem(){
     let yourOrder = ''
-    for(let menu of menuArray){
-    yourOrder += `  
-                <div class="order-item-name">
-                    <h3>${menu.name}</h3>
-                    <button id="remove-btn-${menu.id}" class="remove">remove</button>
+        
+    itemArray.forEach(function(menu){
+    yourOrder += `<div class="order-items" id="order-items">
+                <div class="items">
+                    <div class="order-item-name">
+                        <h3>${menu.name}</h3>
+                        <button id="remove-btn-${menu.id}" class="remove" data-remove-btn="${menu.id}">remove</button>
+                    </div>
+                    <div class="item-price">
+                        <h4 class="menu-count">x ${menu.quantity}</h4>
+                        <h4 class="menu-price">$${menu.price*menu.quantity}</h4>
+                    </div>
                 </div>
-                <h4 class="menu-count">${menu.quantity}x</h4>
-                <h4 class="menu-price">${menu.price*menu.quantity}$</h4>`
+                </div>
+                `
 
-    }
-    document.getElementById('order-item').innerHTML = yourOrder
-    
+    })
+        return yourOrder    
 }
+
+// function getTotalPrice(item){
+//     itemArray.filter(function(menu){
+//         totalPrice -= itemArray.price*itemArray.quantity;
+//     })
+// }
+
+
+
+function renderOrder(){
+    document.getElementById('order-item').innerHTML = getItem()
+
+ }
+ renderOrder()
+
      
             
 function renderMenu(){
@@ -60,7 +142,7 @@ function renderMenu(){
                 </div>
             </div>
             <div class="item-icon">
-                <button id="icon-btn" class="icon-btn" data-btn="${menu.id}">
+                <button id="icon-btn" class="icon-btn" data-add-btn="${menu.id}">
                     <i class="fa-thin fa-plus"></i>
                 </button>
             </div>
